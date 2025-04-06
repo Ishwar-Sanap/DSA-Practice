fe0f55c9508c143eb9d5f76c057e72949c6b42c6
Ishwar-Sanap <ishwarsanap512@gmail.com>
2025-04-06 23:40:01 +0530

    added problem description and link

--- Arrays/Hard/Count_Reverse_pairs.cpp
+++ Arrays/Hard/Count_Reverse_pairs.cpp
@@ -1,15 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Ex: [1,2,3,4, 0,1,7] output ---> 6
-
 /*
+https://leetcode.com/problems/reverse-pairs/description/
+
+Given an integer array nums, return the number of reverse pairs in the array.
+
+A reverse pair is a pair (i, j) where:
+
+0 <= i < j < nums.length and
+nums[i] > 2 * nums[j].
+
 Input: nums = [2,4,3,5,1]
 Output: 3
 Explanation: The reverse pairs are:
 (1, 4) --> nums[1] = 4, nums[4] = 1, 4 > 2 * 1
 (2, 4) --> nums[2] = 3, nums[4] = 1, 3 > 2 * 1
 (3, 4) --> nums[3] = 5, nums[4] = 1, 5 > 2 * 1
+
+// Ex: [1,2,3,4, 0,1,7] output ---> 6
 */
 int getReversePairCnt(vector<int> &arr, int si, int mid, int ei)
 {
